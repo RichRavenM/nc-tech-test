83e2c98f7fea61909ab707ae8e59c48f7a744290
RichRavenM <richard.s.morris@btinternet.com>
2023-11-15 21:58:45 +0000

    modifies GeneralCard for deleting and posting pupropses

--- src/__tests__/GeneralCard.test.js
+++ src/__tests__/GeneralCard.test.js
@@ -11,4 +11,18 @@ describe("GeneralCard", () => {
     expect(TestCard.imageUrl).toBe("http://image.com");
     expect(TestCard.card_id).toBe("card009");
   });
+  it("GeneralCard has static totalCards property", () => {
+    expect(GeneralCard.totalCards).toBe(3);
+    const TestCard = new GeneralCard("Title A", "http://image.com", "card009");
+    expect(TestCard.totalCards).toBe(undefined);
+  });
+  it("static increaseTotalCards increases totalCards value by 1 and decreaseTotalCards decreases it by 1", () => {
+    const startVal = GeneralCard.totalCards;
+    GeneralCard.increaseTotalCards();
+    expect(GeneralCard.totalCards).toBe(4);
+    expect(GeneralCard.totalCards - startVal).toBe(1);
+    GeneralCard.decreaseTotalCards();
+    expect(GeneralCard.totalCards).toBe(3);
+    expect(GeneralCard.totalCards - startVal).toBe(0);
+  });
 });
--- src/classes/GeneralCard.js
+++ src/classes/GeneralCard.js
@@ -4,6 +4,19 @@ class GeneralCard {
     this.imageUrl = imageUrl;
     this.card_id = card_id;
   }
+
+  static _totalCards = 3;
+  static get totalCards() {
+    return this._totalCards
+  }
+  static increaseTotalCards() {
+    this._totalCards++;
+    return this._totalCards;
+  }
+  static decreaseTotalCards() {
+    this._totalCards--;
+    return this._totalCards;
+  }
 }
 
 module.exports = { GeneralCard };
